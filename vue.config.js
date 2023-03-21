e470fbbebfd98c3f116ec77efa8167d8fc868d82
Leonid Vegera <leonid.vegera@gmail.com>
2023-03-21 11:15:38 +0200

    Deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   publicPath: process.env.NODE_ENV === 'production'
+//       ? '/memory_game/'
+//       : '/',
+//   transpileDependencies: true
+// })
+
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
       ? '/memory_game/'
-      : '/',
-  transpileDependencies: true
-})
+      : '/'
+}
